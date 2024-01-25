a820e9ce069abc26bc76ec00baae352aeeefc46e
Lívia <liviapnd@gmail.com>
2024-01-25 10:13:18 -0300

    fix/build: adicionando FK VagaId à entidade veículo

--- Migrations/20240125125357_ModificaTabelasVeiculosClientes.Designer.cs
+++ Migrations/20240125125357_ModificaTabelasVeiculosClientes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sistema_estacionamento.Context;
 
@@ -11,9 +12,11 @@ using sistema_estacionamento.Context;
 namespace sistema_estacionamento.Migrations
 {
     [DbContext(typeof(EstacionamentoContext))]
-    partial class EstacionamentoContextModelSnapshot : ModelSnapshot
+    [Migration("20240125125357_ModificaTabelasVeiculosClientes")]
+    partial class ModificaTabelasVeiculosClientes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
